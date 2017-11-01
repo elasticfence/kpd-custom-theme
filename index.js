d84c2ae30782c8e2bb89efae7067982391368c02
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2017-11-01 10:00:05 +0100

    extend with config options

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ module.exports = function (kibana) {
 			hacks: [
 				'plugins/kpd_custom_theme/hacks'
 			]
+		},
+		config: function (Joi) {
+		        return Joi.object({
+ 		          enabled: Joi.boolean().default(true),
+  		          css: Joi.string().default('plugins/kpd_custom_theme/css/style.less'),
+			  favicon: Joi.string().default('./images/logo.png')
+			}).default()
 		}
 	})
 }
